f826c9c4f0041f66979d6a5f433568c31aab6efd
ampaire <femayapr@gmail.com>
2020-02-07 13:24:58 -0600

    Add the model files for comments and likes

---
+++ db/migrate/20200207192319_add_reference_to_comments_and_likes.rb
@@ -0,0 +1,6 @@
+class AddReferenceToCommentsAndLikes < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :posts, :likes, index:true
+    add_reference :posts, :comments, index:true
+  end
+end
