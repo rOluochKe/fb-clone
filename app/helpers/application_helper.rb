8f274d7a7fb1b54c230a41419ea840d56c3890ee
ampaire <femayapr@gmail.com>
2020-02-11 11:51:19 -0600

    Remove logicfrom helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def liked_post?(subject, type)
+    liked =  false
+    liked = Like. where(user_id: current_user.id, post_id: subject.id). exists? if type == "post"
+  end
+  def unlike?( subject, type)
+    
+  end
 end
