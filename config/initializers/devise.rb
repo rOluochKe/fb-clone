2d9ef5746b7bdd34c81e96c85ed7f540b749b28c
stickler-ci <support@stickler-ci.com>
2020-02-27 13:57:18 +0000

    Fixing style errors.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, '887169088377385', '13ad8a81c74ce018bee8970cd1748655',token_params: { parse: :json }
+  config.omniauth :facebook, '887169088377385', '13ad8a81c74ce018bee8970cd1748655', token_params: { parse: :json }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
