574d44ca81f1363e38956a8a83787d2d3e8325bc
Raymond Oluoch <rasholuoch@gmail.com>
2020-02-28 18:56:00 +0300

    adding privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'privacy/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   root to: 'home#index'
   resources :users
   resources :friendships, only: %i[index create update destroy unfriend]
   delete 'friendship', to: 'friendships#unfriend'
+  get 'privacy', to: 'privacy#index'
   # Post Routes
   resources :posts, only: %i[create destroy index show update edit] do
     resources :likes, only: %i[create destroy]
