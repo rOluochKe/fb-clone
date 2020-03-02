995c8d1f3a4a6081a85b95344cc5e3411237ed50
Raymond Oluoch <rasholuoch@gmail.com>
2020-03-03 00:57:36 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'privacy/index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
   resources :users
   resources :friendships, only: %i[index create update destroy unfriend]
   delete 'friendship', to: 'friendships#unfriend'
   get 'privacy', to: 'privacy#index'
+  get 'terms', to: 'terms#index'
   # Post Routes
   resources :posts, only: %i[create destroy index show update edit] do
     resources :likes, only: %i[create destroy]
