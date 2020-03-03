485bfd0a3ce6ad3ac50f7f2a9715046216a50065
Raymond Oluoch <rasholuoch@gmail.com>
2020-03-03 17:23:53 +0300

    fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  # before_save { self.email = email.downcase }
-  # validates :name, presence: true, length: { maximum: 20 }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  # validates :email, presence: true, length: { maximum: 255 },
-  #                   format: { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   # validates :password, presence: true, length: { minimum: 6 }
   # validates :password_confirmation, presence: true, length: { minimum: 6 }
 
