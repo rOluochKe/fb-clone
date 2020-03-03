62ec95c5d52142fe3cf312fbe9dd20d1d6d4494f
Raymond Oluoch <rasholuoch@gmail.com>
2020-03-03 17:29:05 +0300

    password auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  # validates :password, presence: true, length: { minimum: 6 }
-  # validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
 
   has_many :posts
   has_many :comments
@@ -46,8 +46,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+      user.password_confirmation = user.password
       user.name = auth.info.name # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
     end
   end
 end
