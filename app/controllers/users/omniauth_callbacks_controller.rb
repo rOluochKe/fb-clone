a96e0fa0e42f67038775dfc26df1a65fc0d025c3
Raymond Oluoch <rasholuoch@gmail.com>
2020-03-04 00:44:38 +0300

    refactoring facebook auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to '/users/auth/facebook?auth_type=rerequest&scope=email'
       return # be sure to include an return if there is code after this otherwise it will be executed
     end
-    
+
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
